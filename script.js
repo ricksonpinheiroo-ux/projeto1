0f615f27f8fe8d22e80d4e401301c313389f0251
Rickson <ricksonpinheiroo@gmail.com>
2026-05-02 19:39:10 -0300

    Correção de bugs re adicionado integração

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const login = document.getElementById("login");
 const senha = document.getElementById("senha");
 
-if (login.value === "admin" && senha.value === "root") {
+function validar() {
+    event.preventDefault();
+
+  if (login.value === "admin" && senha.value === "root") {
     alert("O login foi efetuado com sucesso!");
-    window.location.href = "./pag.html";
-} else {
+    window.location.href = "pag.html";
+  } else {
     alert("Tem algo de errado, tente novamente!");
+  }
 }
