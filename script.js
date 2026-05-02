ba1d3f3a17b4102dab91f4ae4c86a74f80426127
Rickson <ricksonpinheiroo@gmail.com>
2026-05-02 20:23:09 -0300

    Função de cadastrar usuarios

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const login = document.getElementById("login");
 const senha = document.getElementById("senha");
+const lista = document.getElementById("listaUsuarios");
 
 function validar() {
     event.preventDefault();
@@ -10,4 +11,27 @@ function validar() {
   } else {
     alert("Tem algo de errado, tente novamente!");
   }
+}
+
+function adicionar() {
+    event.preventDefault();
+    
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+
+  if (nome || email === "") {
+    alert('Preencha todos os campos corretamente!');
+    return;
+  }
+  
+  const usuario ={
+    nome: nome,
+    email: email,
+  }
+  const li = document.createElement("li");
+  li.textContent = `Nome: ${usuario.nome} - Email: ${usuario.email}`;
+  lista.appendChild(li);
+
+  document.getElementById("nome").value = "";
+  document.getElementById("email").value = "";
 }
